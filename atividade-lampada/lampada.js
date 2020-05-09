5fa23968ac729a8ce2f788095416d03a8df1a099
Vailton Carlitos Gomes <55075180+Vailton@users.noreply.github.com>
2020-05-08 22:10:12 -0300

    lampada 3

--- atividade-lampada/lampada.js
+++ atividade-lampada/lampada.js
@@ -1,18 +1,63 @@
-function ligar() {
 
-    //elemento imagem
-    const lampada = document.querySelector(".lampada") 
-
+//function ligar() {
+    // pegar elemento de imagem
+    //const lampada = document.querySelector("#lampada")
     //trocar src para ligada
-    lampada.src = "img/img/lampada-ligada.jpg";
-
+    //lampada.src = "img/lampada-ligada.jpg"
     //trocar alt para ligada
-    lampada.alt = "Lâmpada ligada"
+    //lampada.alt = "Lâmpada ligada"
 
-    //pegar elemento button
-    const botaoLigar = document.querySelector("#botao-ligar")
-
+    //pegar elemento button de ligar
+    //const botaoLigar = document.querySelector("#botao-ligar")
     //adicionar atributo disabled
-    botaoLigar.disabled = true
-    //pegar elemento button 
-}   
+    //botaoLigar.disabled = true
+
+    //pegar elemento button de desligar
+    //const botaoDesligar = document.querySelector("#botao-desligar")
+    //remover atributo disabled
+   //botaoDesligar.disabled = false
+//}
+
+//function desligar() {
+    // pegar elemento de imagem
+    //const lampada = document.querySelector("#lampada")
+    //trocar src para desligada
+    //lampada.src = "img/lampada-desligada.jpg"
+    //trocar alt para desligada
+    //lampada.alt = "Lâmpada desligada"
+
+    //pegar elemento button de ligar
+    //const botaoLigar = document.querySelector("#botao-ligar")
+    //remover atributo disabled
+   //botaoLigar.disabled = false
+
+    //pegar elemento button de desligar
+    //const botaoDesligar = document.querySelector("#botao-desligar")
+    //adicionar atributo disabled
+    //botaoDesligar.disabled = true
+//}
+
+
+const lampada = document.querySelector("#lampada")
+
+function interruptor(button){
+    console.log("conteudo inteiro ", button)
+    console.log("propriedade value", button.value)
+    console.log("propriedade textContent", button.textContent)
+    
+    if (button.value === "ligar" ){        
+        lampada.src = "img/lampada-ligada.jpg"
+        lampada.alt = "lapada ligada"
+        button.textContent = "Desligar"
+        button.value = "desligar"
+    }
+    else{
+        lampada.src = "img/lampada-desligada.jpg"
+        lampada.alt = "lapada desligada"
+        button.textContent = "Ligar"
+        button.value = "ligar"
+    }
+
+    //console.log(button.value)
+    //console.log(button.textContent)
+}
