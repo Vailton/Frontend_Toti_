1e3561792187436f1e2272b46f8ef45ab4b692a9
Vailton Carlitos Gomes <55075180+Vailton@users.noreply.github.com>
2020-05-10 15:15:27 -0300

    funcition 1 js

---
+++ lista-de-tarefas/lista.js
@@ -0,0 +1,9 @@
+
+
+//var data = new Date();
+//alert(data);
+
+function clicou(){
+    document.getElementsByClassName("task-list");
+    console.log(document.getElementsByClassName("task-list__item"));
+}
