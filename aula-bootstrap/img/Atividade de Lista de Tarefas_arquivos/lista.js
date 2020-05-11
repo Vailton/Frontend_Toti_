4e90764ad2b359bfd9df7ae857d0539e6d248aae
Vailton Carlitos Gomes <55075180+Vailton@users.noreply.github.com>
2020-05-11 18:54:37 -0300

    primeiro teste de funcionalidade

---
+++ aula-bootstrap/img/Atividade de Lista de Tarefas_arquivos/lista.js
@@ -0,0 +1,23 @@
+
+
+//var data = new Date();
+//alert(data);
+
+//function clicou(elemento){
+   // elemento.innerHTML = "OBRIGADO";
+//}
+
+//function funcaoChange(elemento){
+    //alert(elemento);
+    //console.log(elemento.value);
+//}
+
+function clicou(elemento){
+    
+const newTask = document.querySelector("#new-task").Value;
+const liElement = document.createElement("li");
+liElement.textContent = newTask;
+
+}
+
+
--- lista-de-tarefas/lista.js
+++ lista-de-tarefas/lista.js
@@ -3,21 +3,27 @@
 //var data = new Date();
 //alert(data);
 
-//function clicou(elemento){
-   // elemento.innerHTML = "OBRIGADO";
-//}
 
 //function funcaoChange(elemento){
     //alert(elemento);
     //console.log(elemento.value);
 //}
 
-function clicou(elemento){
-    
-const newTask = document.querySelector("#new-task").Value;
-const liElement = document.createElement("li");
-liElement.textContent = newTask;
+function clicou(){
+
+    var nome = document.querySelector("#digite").value;
+    var entrada = document.querySelector("#lista");
+      
+    var  liNovo = document.createElement("li");
+    var txtNovo = document.createTextNode(nome);
+
+
+    liNovo.appendChild(txtNovo);   
+    entrada.appendChild(liNovo);
 
 }
+//
+//
+
 
 
