1bc5e2b7c2b6381fb909e39c5c022453a6594633
Vailton Carlitos Gomes <55075180+Vailton@users.noreply.github.com>
2020-05-13 20:00:21 -0300

    Lista de exercício 13/05

--- lista-de-tarefas/lista.js
+++ lista-de-tarefas/lista.js
@@ -1,30 +1,46 @@
+var digiteTarefaInput = document.querySelector("#adicionar")
+var botaoAdicionar = document.querySelector("#add-task-button")
+var listaDeTarefas = document.querySelector(".task-list")
 
-
-//var data = new Date();
-//alert(data);
-
-
-//function funcaoChange(elemento){
-    //alert(elemento);
-    //console.log(elemento.value);
-//}
-
-function clicou(){
-
-    var nome = document.querySelector("#digite").value;
-    var entrada = document.querySelector("#lista");
-      
-    var  liNode = document.createElement("li");
-    var txtNode = document.createTextNode(nome);
-
-
-    liNode.appendChild(txtNode);   
-    entrada.appendChild(liNode);
+function adicionarTarefa(event) {  
+    if (digiteTarefaInput.value === ""){
+        return;
+    }
+    const novoItem = document.createElement("li")
+    const novoButton = document.createElement("button")    
     
+    novoItem.textContent = digiteTarefaInput.value
+    novoButton.className = botaoAdicionar.classList
 
+    novoButton.classList.add("task-list__item") 
+    novoItem.classList.add("task-list__item--done")    
+    
+    listaDeTarefas.appendChild(novoItem) 
+    listaDeTarefas.appendChild(novoButton)
+    
+    digiteTarefaInput.value = ""   
+    digiteTarefaInput.focus()
+
+    function clique(){
+        const botao = document.querySelector("#add-task-button")
+        botao.addEventListener("click", clique) 
+        botao.onclick = clique      
+    }
+    
+    function duploClique(){
+        const botao1 = document.querySelector("#add-task-button")       
+        botao1.addEventListener("dblclick",duploClique)
+        botao1.dblclick = duploClique
+
+    }
+
+    function digitou(){
+        const botao2 = document.querySelector("#add-task-button")        
+        botao2.addEventListener("keydown", digitou)
+        botao3.keydown = digitou
+    }
+
+    
 }
-//
-//
 
-
-
+botaoAdicionar.addEventListener("click", adicionarTarefa)
